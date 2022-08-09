b0bf6c9ab18d06ea0c373ad3a4106b2a39eeaf2f
saja.abufarha <saja.abufarha@IC-CS233-21.uc.ucalgary.ca>
2022-08-09 17:18:49 -0600

    Created MenSectionTotal class
    This is a sub class of Total, and uses the methods in Total to get the
    total for the men section.
    I also added labels in the MenSectionScene to display the total and the
    error message.

--- src/application/OnlineShopController.java
+++ src/application/OnlineShopController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -17,60 +18,46 @@ public class OnlineShopController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
-	
+
 	@FXML
-    private TextField labyrinthTextfield;
-
-    @FXML
-    private Button add;
-
-    @FXML
-    private Label pearlPrice;
-
-    @FXML
-    private TextField fahadTextfield;
-
-    @FXML
-    private TextField travellerTextField;
-
-    @FXML
-    private Label talismanPrice;
-
-    @FXML
-    private Label travellerPrice;
-
-    @FXML
-    private Label vigorPrice;
-
-    @FXML
-    private TextField talismanTextfield;
-
-    @FXML
-    private Label fahadPrice;
-
-    @FXML
-    private TextField vigorTextfield;
-
-    @FXML
-    private TextField pearlTextfield;
-
-    @FXML
-    private Label labyrinthPrice;
+	private TextField labyrinthTextfield;
+	@FXML
+	private Button add;
+	@FXML
+	private Label pearlPrice;
+	@FXML
+	private TextField fahadTextfield;
+	@FXML
+	private TextField travellerTextField;
+	@FXML
+	private Label talismanPrice;
+	@FXML
+	private Label travellerPrice;
+	@FXML
+	private Label vigorPrice;
+	@FXML
+	private TextField talismanTextfield;
+	@FXML
+	private Label fahadPrice;
+	@FXML
+	private TextField vigorTextfield;
+	@FXML
+	private TextField pearlTextfield;
+	@FXML
+	private Label labyrinthPrice;
 
 	@FXML
 	private Button checkoutButton;
-
 	@FXML
 	private Button womenButton;
-
 	@FXML
 	private Button menButton;
-
 	@FXML
 	private Button addItems;
 	@FXML
 	private Label errorLabel;
-
+	@FXML
+    private Label totaldisplay;
 	public void switchToMainScene(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("OnlineShopFirstView.fxml"));
 		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -109,14 +96,17 @@ public class OnlineShopController {
 
 	@FXML
     void addItems(ActionEvent event) {
-    	errorLabel.setText("");
-    	double totalCheckout = 0.0;
-    	//System.out.print(Double.parseDouble(labyrinthPrice.getText())* Double.parseDouble(labyrinthTextfield.getText()));
-    	Total labyrinth = new Total(labyrinthTextfield.getText() ,Double.parseDouble(labyrinthPrice.getText()) );
-    	errorLabel.setText(labyrinth.setValue(labyrinthTextfield.getText()));
 
-    	//errorLabel.setText(projectGrade.setValue(projectGradeTextField.getText())
-    	System.out.print(labyrinth.itemTotal());
+    	MenSectionTotal men = new MenSectionTotal(labyrinthTextfield.getText(), fahadTextfield.getText(), travellerTextField.getText()
+    	, talismanTextfield.getText(), vigorTextfield.getText(), pearlTextfield.getText()
+    	, Double.parseDouble(labyrinthPrice.getText()), Double.parseDouble(fahadPrice.getText()),Double.parseDouble(travellerPrice.getText())
+    	,Double.parseDouble(talismanPrice.getText()), Double.parseDouble(vigorPrice.getText()),Double.parseDouble(pearlPrice.getText()));
+    	
+    	men.setErrorMessageMen();
+    	totaldisplay.setText(String.format("Your total for the men section is %.02f", men.getTotalMen()));
+    	errorLabel.setText(men.getErrorMessageMen());
 
     }
+	
+	
 }
